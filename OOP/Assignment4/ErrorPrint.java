34e9d18c98a9e105b449ae64fb0cb0433e1a2940
Akshat Mathur <akshat.mathur.com>
2015-08-24 10:23:45 +0530

    OOPs assignment4

---
+++ OOP/Assignment4/ErrorPrint.java
@@ -0,0 +1,13 @@
+/**
+ * Class that displays error message whenever an error occurs
+ * @author Akshat
+ *
+ */
+public class ErrorPrint {
+
+	public static void print()
+	{
+		System.out.println("Enter a valid input from available options");      //Error message to be printed
+	}
+
+}
---
+++ OOP/Assignment4/PassengerTrain.java
@@ -0,0 +1,34 @@
+/**
+ * Class to store passenger train information
+ * 
+ * @author Akshat
+ *
+ */
+public class PassengerTrain extends Train {
+
+	String type;
+	String seats;
+
+	public PassengerTrain(String trainId, String type, String from, String to,
+			String seats, String travelTime, String price) {
+		super(trainId, from, to, travelTime, price);
+		this.seats = seats;
+		this.type = type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setWeight(String weight) {
+		this.seats = weight ;
+	}
+
+	public String getSeats() {
+		return seats;
+	}
+}
