d0a363da4af1bca0daa87673d4ef7987952c36b1
Akshat Mathur <akshat.mathur.com>
2015-10-21 18:08:50 +0530

    Design Pattern Session 2

---
+++ Web/Design Pattern/DesignPattern2/src/com/Assignment2/Mentor.java
@@ -0,0 +1,16 @@
+package com.Assignment2;
+/**
+ *The 'ConcreteHandler' class
+ *
+ */
+public class Mentor extends LeaveApprover {
+
+	@Override
+	public void ProcessRequest(int leave) {
+		if (leave <= 1) {
+			System.out.println("Leave approved by Mentor");
+		} else if (successor != null) {
+			successor.ProcessRequest(leave);
+		}
+	}
+}
