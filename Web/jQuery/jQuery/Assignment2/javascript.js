7057cb4b8ad52ac6682df1b8b2be794aca95ca16
Akshat Mathur <akshat.mathur.com>
2015-10-29 18:35:24 +0530

    assignment 1 jQuery

---
+++ Web/jQuery/jQuery/Assignment2/javascript.js
@@ -0,0 +1,9 @@
+function showHide() {
+				if ($('#Details').is(":visible")) {
+						$('#Details').hide();
+						$('#showHideButton').val('SHOW');
+				} else {
+						$('#Details').show();
+						$('#showHideButton').val('HIDE');
+					}
+			}
